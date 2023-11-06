e9ae4b95a55c8b9c0aa13ab22d9a5f0362e0ab0b
PlayDay <18056374+playday3008@users.noreply.github.com>
2023-11-06 23:26:08 +0100

    Update pins_board.h

--- variants/standard/pins_board.h
+++ variants/standard/pins_board.h
@@ -11,12 +11,6 @@
 #define PIN_SW_4    (3)
 #define PIN_SW_5    (4)
 
-// Potentiometer 1 (POT1)
-#define PIN_POT_1 (A0)
-
-// Potentiometer 2 (POT2)
-#define PIN_POT_2 (A1)
-
 // 5-Way Tactile Switch (JOY)
 #define PIN_JOY_MODE  (INPUT_PULLUP)
 #define PIN_JOY_A     (0)
@@ -29,10 +23,9 @@
 #define PIN_JOY_LEFT  (PIN_JOY_C)
 #define PIN_JOY_RIGHT (PIN_JOY_B)
 
-// RGB LED (RGB)
-#define PIN_RGB_R (12)
-#define PIN_RGB_G (13)
-#define PIN_RGB_B (15)
+// Potentiometers (POT1 and POT2)
+#define PIN_POT_1 (A0)
+#define PIN_POT_2 (A1)
 
 // 8-channel Darlington Sink Driver (ULN) (ULN2803APG)
 #define PIN_ULN_1 (8)
@@ -44,15 +37,13 @@
 #define PIN_ULN_7 (14)
 #define PIN_ULN_8 (15)
 
-// LCD 16x2 Parallel (LCD) (4bit mode)
-#define PIN_LCD_RS (20)
-#define PIN_LCD_RW (21)
-#define PIN_LCD_EN (22)
-#define PIN_LCD_D4 (28)
-#define PIN_LCD_D5 (29)
-#define PIN_LCD_D6 (30)
-#define PIN_LCD_D7 (31)
-#define PIN_LCD_BL (23)
+// RGB LED (RGB)
+#define PIN_RGB_R (12)
+#define PIN_RGB_G (13)
+#define PIN_RGB_B (15)
+
+// Buzzer (BUZ)
+#define PIN_BUZ (27) // A3
 
 // Double 7-Segment Display (7LED) (7SEG)
 #define PIN_7SEG_A  (31)
@@ -66,22 +57,29 @@
 #define PIN_7SEG_D1 (19)
 #define PIN_7SEG_D2 (18)
 
-// Buzzer (BUZ)
-#define PIN_BUZ (27) // A3
+// LCD 16x2 Parallel (LCD) (4bit mode)
+#define PIN_LCD_RS (20)
+#define PIN_LCD_RW (21)
+#define PIN_LCD_EN (22)
+#define PIN_LCD_D4 (28)
+#define PIN_LCD_D5 (29)
+#define PIN_LCD_D6 (30)
+#define PIN_LCD_D7 (31)
+#define PIN_LCD_BL (23)
 
-// 2-Wire Serial Temperature Sensor (TMP) (I2C) (TCN75AV)
-#define PIN_TMP_SDA  (17)
-#define PIN_TMP_SCL  (16)
-#define PIN_TMP_ADDR (0b1001111)
+// Clock and calendar with 240 x 8-bit RAM (RTC) (I2C) (PCF8583)
+#define PIN_RTC_SDA  (17)
+#define PIN_RTC_SCL  (16)
+#define PIN_RTC_ADDR (0b1010000)
 
 // 12-Bit Voltage Output Digital-to-Analog Converter with SPI Interface (DAC) (SPI) (MCP4921)
 #define PIN_DAC_SCK  (7)
 #define PIN_DAC_SDI  (5)
 #define PIN_DAC_VOUT (15)
 
-// Clock and calendar with 240 x 8-bit RAM (RTC) (I2C) (PCF8583)
-#define PIN_RTC_SDA  (17)
-#define PIN_RTC_SCL  (16)
-#define PIN_RTC_ADDR (0b1010000)
+// 2-Wire Serial Temperature Sensor (TEMP) (I2C) (TCN75AV)
+#define PIN_TEMP_SDA  (17)
+#define PIN_TEMP_SCL  (16)
+#define PIN_TEMP_ADDR (0b1001111)
 
 #endif // Pins_Board_h
